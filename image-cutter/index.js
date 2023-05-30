0da761dbe4a5373fb8937127e699ca1b861112d6
yaroslav.nizhankovskyi@gmail.com <yaroslav.nizhankovskyi@gmail.com>
2023-05-30 16:16:46 +0300

    init

---
+++ image-cutter/index.js
@@ -0,0 +1,42 @@
+const sharp = require('sharp');
+
+module.exports = async function (context, message) {  
+    context.log(message);
+    context.log(JSON.parse(message).url);
+    const {url, sizes = [100,300]} = JSON.parse(message);
+    const origin = await downloadFile(url);        
+
+    const pormises = sizes.map(size => {
+        return sharp(origin)
+            .resize(size, size,
+                {
+                    fit: sharp.fit.contain,
+                })
+            .toBuffer({resolveWithObject: true})
+    });
+
+    let results = [];
+    try{
+        results = await Promise.all(pormises);
+    }
+    catch(exception){
+        context.log.error(exception);
+    }
+
+    context.res = {
+        queueOutput: results
+    };
+}
+
+async function downloadFile(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Download failed');
+      }
+      return await response.arrayBuffer();
+    } catch (error) {
+      context.log.error('Error:', error);
+      return null;
+    }
+  }
